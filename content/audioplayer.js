56fba3ab8db4d25b2e4b8c31e675b2a9e3068a7a
staffan <staffanh@ru.is>
2022-06-28 13:12:14 +0000

    tab size

--- content/audioplayer.js
+++ content/audioplayer.js
@@ -1,85 +1,85 @@
 class AudioPlayer {
-    constructor() {
-        this.mediaSource = null;
-        this.audio = new Audio();
-        this.playbackRate = 1;
-        this.text = '';
-        this.voice = '';
-        this.sourceBuffer = undefined;
-        this.requests = [];
-        this.first = true;
+  constructor() {
+    this.mediaSource = null;
+    this.audio = new Audio();
+    this.playbackRate = 1;
+    this.text = '';
+    this.voice = '';
+    this.sourceBuffer = undefined;
+    this.requests = [];
+    this.first = true;
+  }
+
+  compareText = (text) => {
+    return this.text == text;
+  };
+
+  play = () => {
+    if (!this.isPlaying()) this.audio.play();
+  };
+
+  pause = () => {
+    this.audio.pause();
+  };
+
+  stop = () => {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+  };
+
+  isPlaying = () => {
+    return !this.audio.paused;
+  };
+
+  setupPlayer = (requests, text, playbackRate, voice) => {
+    this.stop();
+    this.mediaSource = new MediaSource();
+    this.audio.src = window.URL.createObjectURL(this.mediaSource);
+    this.requests = requests;
+    this.setPlaybackRate(playbackRate);
+    this.text = text;
+    this.voice = voice;
+    this.first = true;
+
+    this.mediaSource.addEventListener('sourceopen', this.openSource);
+  };
+
+  openSource = async (_) => {
+    this.sourceBuffer = this.mediaSource.addSourceBuffer('audio/mpeg');
+
+    for (const request of this.requests) {
+      const response = await fetch(request.url, request.content);
+      const reader = response.body.getReader();
+      await this.stream(reader);
     }
+    this.mediaSource.endOfStream();
+  };
 
-    compareText = (text) => {
-        return this.text == text;
-    };
+  stream = async (reader) => {
+    let streamNotDone = true;
 
-    play = () => {
-        if (!this.isPlaying()) this.audio.play();
-    };
+    while (streamNotDone) {
+      const { value, done } = await reader.read();
+      if (done) {
+        streamNotDone = false;
+        break;
+      }
 
-    pause = () => {
-        this.audio.pause();
-    };
+      await new Promise((resolve, reject) => {
+        this.sourceBuffer.appendBuffer(value);
+        this.sourceBuffer.onupdateend = () => {
+          if (this.first) {
+            this.play();
+            this.first = false;
+          }
+          resolve(true);
+        };
+      });
+    }
+  };
 
-    stop = () => {
-        this.audio.pause();
-        this.audio.currentTime = 0;
-    };
-
-    isPlaying = () => {
-        return !this.audio.paused;
-    };
-
-    setupPlayer = (requests, text, playbackRate, voice) => {
-        this.stop();
-        this.mediaSource = new MediaSource();
-        this.audio.src = window.URL.createObjectURL(this.mediaSource);
-        this.requests = requests;
-        this.setPlaybackRate(playbackRate);
-        this.text = text;
-        this.voice = voice;
-        this.first = true;
-
-        this.mediaSource.addEventListener('sourceopen', this.openSource);
-    };
-
-    openSource = async (_) => {
-        this.sourceBuffer = this.mediaSource.addSourceBuffer('audio/mpeg');
-
-        for (const request of this.requests) {
-            const response = await fetch(request.url, request.content);
-            const reader = response.body.getReader();
-            await this.stream(reader);
-        }
-        this.mediaSource.endOfStream();
-    };
-
-    stream = async (reader) => {
-        let streamNotDone = true;
-
-        while (streamNotDone) {
-            const { value, done } = await reader.read();
-            if (done) {
-                streamNotDone = false;
-                break;
-            }
-
-            await new Promise((resolve, reject) => {
-                this.sourceBuffer.appendBuffer(value);
-                this.sourceBuffer.onupdateend = () => {
-                    if (this.first) {
-                        this.play();
-                        this.first = false;
-                    }
-                    resolve(true);
-                };
-            });
-        }
-    };
-
-    setPlaybackRate = (playbackRate) => {
-        this.playbackRate = playbackRate;
-        this.audio.playbackRate = playbackRate;
-    };
+  setPlaybackRate = (playbackRate) => {
+    this.playbackRate = playbackRate;
+    this.audio.playbackRate = playbackRate;
+  };
 }
