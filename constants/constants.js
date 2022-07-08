73f86b595e67a788b101efe444f82688f3f7ee72
staffan <staffanh@ru.is>
2022-07-08 10:56:10 +0000

    add volume, remove free update to single quotes

--- constants/constants.js
+++ constants/constants.js
@@ -1,31 +1,31 @@
 const CONTENT_COMMANDS = {
-  PLAY: "play",
-  PAUSE: "pause",
-  IS_PLAYING: "is_playing",
-  STOP: "stop",
-  MESSAGE: "message",
-  CHANGE_PLAYBACK_RATE: "change_playback_rate",
-  GET_PLAYBACK_RATE: "get_playback_rate",
-  UPDATE_VALUE: "update_value",
+  PLAY: 'play',
+  PAUSE: 'pause',
+  IS_PLAYING: 'is_playing',
+  STOP: 'stop',
+  MESSAGE: 'message',
+  CHANGE_PLAYBACK_RATE: 'change_playback_rate',
+  GET_PLAYBACK_RATE: 'get_playback_rate',
+  UPDATE_VALUE: 'update_value',
 };
 
 const BACKGROUND_COMMANDS = {
-  TTS: "tts",
-  MESSAGE: "message",
+  TTS: 'tts',
+  MESSAGE: 'message',
 };
 
 const RECEIVERS = {
-  BACKGROUND: "background_script",
-  CONTENT: "content_script",
-  POPUP: "popup_script",
+  BACKGROUND: 'background_script',
+  CONTENT: 'content_script',
+  POPUP: 'popup_script',
 };
 
-const WEBRICE_AUDIO_ID = "webrice_audio";
+const WEBRICE_AUDIO_ID = 'webrice_audio';
 
 const WEBRICE_KEYS = {
-  VOICE: "webrice_voice",
-  FREE_TEXT: "webrice_free_text",
-  PITCH: "webrice_pitch",
-  PITCH_DEFAULT: "webrice_default",
-  SUBSTITUTIONS: "webrice_substitutions",
+  VOICE: 'webrice_voice',
+  PITCH: 'webrice_pitch',
+  PITCH_DEFAULT: 'webrice_default',
+  SUBSTITUTIONS: 'webrice_substitutions',
+  VOLUME: 'webrice_volume',
 };
