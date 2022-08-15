afe339efd8e21c6f2cc36ce590e6dd8a5374976a
staffan <staffanh@ru.is>
2022-08-15 15:14:45 +0000

    add polly support content

--- content/fetch.js
+++ content/fetch.js
@@ -1,12 +1,13 @@
-const DEFAULT_VOICE = "Alfur";
+const DEFAULT_VOICE = 'Alfur';
 const SPECIAL_VOICES = [
-  "Alfur",
-  "Dilja",
-  "Alfur_v2",
-  "Dilja_v2",
-  "Rosa",
-  "Bjartur",
+  'Alfur',
+  'Dilja',
+  'Alfur_v2',
+  'Dilja_v2',
+  'Rosa',
+  'Bjartur',
 ];
+const AWS_VOICES = ['Dora', 'Karl'];
 const MAX_REQUEST_SIZE = 300;
 
 /**
@@ -18,17 +19,17 @@ const MAX_REQUEST_SIZE = 300;
  */
 const normalizeText = (text, specialTrim = false) => {
   let trimmed = text
-    .replace(/([\.\:\,\:]{0,1})\s{0,}\n+/gm, ". ") // replace all newlines and other special whitespaces with ". "
-    .replace(/\s+/gm, " "); // replace multiple spaces left with a single space
+    .replace(/([\.\:\,\:]{0,1})\s{0,}\n+/gm, '. ') // replace all newlines and other special whitespaces with ". "
+    .replace(/\s+/gm, ' '); // replace multiple spaces left with a single space
 
   // For non-amazon voice extra care is needed
   if (specialTrim) {
     trimmed = trimmed
-      .replace(/(?<=\d)\s(?=\d)/gm, ", ") // replace digit space digit with digit comma space digit
-      .replace(/(?<=[\?\!])\./gm, ""); // replace remove dot from ?. or !.
+      .replace(/(?<=\d)\s(?=\d)/gm, ', ') // replace digit space digit with digit comma space digit
+      .replace(/(?<=[\?\!])\./gm, ''); // replace remove dot from ?. or !.
 
     // If first characters are ". " then bad sound trim these away if so
-    if (trimmed.slice(0, 2) == ". ") {
+    if (trimmed.slice(0, 2) == '. ') {
       trimmed = trimmed.slice(2);
     }
   }
@@ -39,7 +40,7 @@ const normalizeText = (text, specialTrim = false) => {
 
   const output = [];
   while (trimmed.length > MAX_REQUEST_SIZE) {
-    const lastSpace = trimmed.lastIndexOf(" ", MAX_REQUEST_SIZE);
+    const lastSpace = trimmed.lastIndexOf(' ', MAX_REQUEST_SIZE);
     const section = trimmed.substring(0, lastSpace);
     output.push(section);
     trimmed = trimmed.slice(lastSpace);
@@ -55,38 +56,49 @@ const normalizeText = (text, specialTrim = false) => {
  * @returns an array of requests, {url, content}
  */
 const getRequestHeaderAndContent = (text, settings) => {
-  const url = "https://tts.tiro.is/v0/speech";
-  const audioType = "mp3";
+  const audioType = 'mp3';
   const voiceName = settings?.voice ? settings.voice : DEFAULT_VOICE;
   const specialTrim = SPECIAL_VOICES.includes(voiceName);
-  const normalizedTexts = normalizeText(text, specialTrim)
-  
-  const requests = normalizedTexts.map((text) => {
-    const request = {
-      url,
-      content: {
-        
-          method: "POST",
-          mode: "cors",
-          headers: {
-            accept: "audio/mpeg",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            Engine: "standard",
-            LanguageCode: "is-IS",
-            LexiconNames: [],
-            OutputFormat: audioType,
-            SampleRate: "16000",
-            SpeechMarkTypes: [],
-            Text: text,
-            TextType: "text",
-            VoiceId: voiceName,
-          }),
-      }
-    }
-    return request;
-  })
+  const normalizedTexts = normalizeText(text, specialTrim);
+  const awsVoice = AWS_VOICES.includes(settings?.voice);
 
-  return requests;
-}
+  const requests = normalizedTexts.map((text) => {
+    const request = awsVoice
+      ? awsRequest(text, audioType, voiceName)
+      : tiroRequest(text, audioType, voiceName);
+    return request;
+  });
+
+  return { backend: awsVoice ? BACKENDS.POLLY : BACKENDS.TIRO, requests };
+};
+
+const tiroRequest = (text, audioType, voiceName) => {
+  const url = 'https://tts.tiro.is/v0/speech';
+
+  return {
+    url,
+    content: {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        accept: 'audio/mpeg',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        Engine: 'standard',
+        LanguageCode: 'is-IS',
+        LexiconNames: [],
+        OutputFormat: audioType,
+        SampleRate: '16000',
+        SpeechMarkTypes: [],
+        Text: text,
+        TextType: 'text',
+        VoiceId: voiceName,
+      }),
+    },
+  };
+};
+
+const awsRequest = (text, audioType, voiceName) => {
+  return pollyParams(text, audioType, voiceName);
+};
